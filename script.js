0e424ee074dbc55341e38520e5527aa8e43b1894
Ifham-Hashir <ifham09052003@gmail.com>
2024-07-01 08:45:09 +0530

    Add functions for basic math operations

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(num1, num2){
+  return num1 + num2;
+}
+
+function sub(num1, num2){
+  return num1 - num2;
+}
+
+function multiply(num1, num2){
+  return num1 * num2;
+}
+
+function div(num1, num2){
+  return num1 / num2;
+}
