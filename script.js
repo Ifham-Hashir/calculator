cf1e7aeeef07f053a8ab32b1a47cc577d3035854
Ifham-Hashir <ifham09052003@gmail.com>
2024-07-01 08:59:09 +0530

    Add the operate function to perform operations

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let firstNum = 0;
+let secondNum = 0;
+let operator = '';
+
 function add(num1, num2){
   return num1 + num2;
 }
@@ -12,4 +16,21 @@ function multiply(num1, num2){
 
 function div(num1, num2){
   return num1 / num2;
+}
+
+function operate(num1, op, num2){
+  switch(op)
+  {
+    case '+':
+      return add(num1, num2);
+
+    case '-':
+      return sub(num1, num2);
+
+    case '*':
+      return multiply(num1, num2);
+
+    case "/":
+      return div(num1, num2);
+  }
 }
